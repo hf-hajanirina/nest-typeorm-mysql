ce297933a62dfc5764ec2d6360c73e8158f84e69
Harimisa Florent <harimisaflorent.mi@gmail.com>
2023-07-13 14:26:16 +0200

    init swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // for using class validator
   app.useGlobalPipes(new ValidationPipe());
+  // for swagger
+  const config = new DocumentBuilder()
+    .setTitle('Nest TypeORM MySQL Starter')
+    .setDescription('Nest TypeORM MySQL Starter API description...')
+    .setVersion('1.0')
+    .addTag('APIs')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
